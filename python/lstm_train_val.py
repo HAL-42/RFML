a72c1ec67584d3ff13ed50bae7be95cba08328da
Yang Xiaobo <hal_42@zju.edu.cn>
2019-07-30 20:07:42 -0700

    Test read performance at lstm_train_val.py

--- python/lstm_train_val.py
+++ python/lstm_train_val.py
@@ -62,9 +62,11 @@ if __name__ == '__main__':
 			train_batches = data_manager.get_train_batches(kBatchSize)
 
 			# iteration
-			process_bar = ProcessBar(batches_num)
+			# process_bar = ProcessBar(batches_num)
+			read_batch_start_time = time.time()
 			for i, train_batch in enumerate(train_batches):
-				process_bar.UpdateBar(i)
+				# process_bar.UpdateBar(i)
+				print("Read Batch Cost {}s".format(time.time() - read_batch_start_time))
 				# get corrupted batch using the un-corrupted data_train
 				batch_X, batch_Y = train_batch
 				batch_X = batch_X.reshape(batch_X.shape[0], lstm_model.TIMESTEPS, -1)
@@ -82,6 +84,8 @@ if __name__ == '__main__':
 				_ = sess.run([optimizer], feed_dict={lstm_model.X: batch_X, lstm_model.Y: batch_Y})
 
 				iteration += 1
+				# Read Batch Start
+				read_batch_start_time = time.time()
 			saver.save(sess, kSnapshotPath, global_step=epoch)
 			logger.info("It Cost {}s to finish this epoch".format(time.time() - epoch_start_time))
 		train_writer.close()
