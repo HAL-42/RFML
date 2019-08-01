ead65bcef0241d8e3b331e9351cd6bc10da5d9f6
Yang Xiaobo <hal_42@zju.edu.cn>
2019-07-31 17:06:58 -0700

    Add lstm_test_model

---
+++ python/lstm_test_model.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@author: Xiaobo Yang
+@contact: hal_42@zju.edu.cn
+@software: PyCharm
+@file: lstm_test_model.py
+@time: 2019/7/31 17:05
+@desc:
+"""
