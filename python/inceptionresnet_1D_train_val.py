2410ff2a9d6c0424837635ff48ff4d4405489a48
Yang Xiaobo <hal_42@zju.edu.cn>
2019-09-04 05:17:20 -0700

    Update inceptionresnet_1D_train_val.py
    1. Use const to manage const
    2. Fix some bug

--- python/inceptionresnet_1D_train_val.py
+++ python/inceptionresnet_1D_train_val.py
@@ -1,13 +1,14 @@
+'''
+@Date: 2019-09-04 00:56:25
+@Author: Xiaobo Yang
+@Email: hal_42@zju.edu.cn
+@Company: Zhejiang University
+@LastEditors: Xiaobo Yang
+@LastEditTime: 2019-09-04 01:00:55
+@Description: 
+'''
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
-@author: Xiaobo Yang
-@contact: hal_42@zju.edu.cn
-@software: PyCharm
-@file: inceptionresnet_1D_train_val.py
-@time: 2019/9/2 13:03
-@desc:
-"""
 import os
 import numpy as np
 import torch
@@ -18,16 +19,18 @@ from inceptionresnet_v2_model.torch_saver import Saver
 from inceptionresnet_v2_model.multi_classification_testor import MultiClassificationTester
 from my_py_tools.my_logger import Logger
 from my_py_tools.my_process_bar import ProcessBar
-import my_py_tools.const as K
 from inceptionresnet_1D_test_model import TestSamples
+
+from my_py_tools.const import Const
+K = Const()
 
 # ! Manual Setting
 # * Path Setting
-K.H5DataDir = os.path.join('..', 'data', 'clean_h5data.diff_module_same_mac_43')
+K.H5DataDir = os.path.join('..', 'data', 'clean_h5data.diff_module_same_mac_mini5')
 K.LogDirComment = ''
 # * Model ,Optimizer and Recover Setting
 K.IsRecover = False
-K.RecoverEpochID = 0
+K.LoadModelNum = -1
 # ** Model Init Setting
 K.ModelSettings = {
     'num_incept_A': 10, 'num_incept_B': 20, 'num_incept_C': 10,
@@ -37,16 +40,17 @@ K.ModelSettings = {
 K.LearningRate = 0.001
 # * Training Setting
 K.BatchSize = 25
-K.NumEpochs = 20
+K.NumEpochs = 2
 # * Log, test and Snapshot Setting
 K.TrainLogInterval = 10
-K.TestLogMultiplier = 20
-K.TestSamplesNum = 5000
-K.SnapshotMultiplier = 2
+K.TestLogMultiplier = 10
+K.SnapshotMultiplier = 3
+K.TestSamplesNum = 1000
 # * Other Setting: Should Use both I+Q or just use I to train
 K.IOnly = True
 # ! Automatic Generated Setting
 K.LogDir = os.path.join('.', 'log', f'torch.{os.path.split(K.H5DataDir)[1]}.ICRS.{K.LogDirComment}.log')
+K.SummaryDir = os.path.join(K.LogDir, 'summary')
 K.TrainValLogFile = os.path.join(K.LogDir, 'train_val.log')
 K.SnapshotFileStr = os.path.join(K.LogDir, 'snapshot', 'InceptionResNet1D-{}.snapshot')
 
@@ -58,77 +62,86 @@ if __name__ == '__main__':
     # * data, log manager and saver, tester
     data_manager = DataManager(K.H5TrainTestDataDir, K.H5ModuleDataDir, I_only=K.IOnly, down_sample=0)
     logger = Logger(K.TrainValLogFile).logger
-    writer = SummaryWriter(K.LogDir)
+    writer = SummaryWriter(K.SummaryDir)
     saver = Saver(K.SnapshotFileStr)
     tester = MultiClassificationTester(data_manager.classes_list)
     # * build OR recover model, optimizer
     # writer.add_graph(net, (dummy_input, ), verbose=True)
     if not K.IsRecover:
-        net = InceptionResNet1D(data_manager.classes_num, num_input_channels=1 if K.IOnly else 2, batch_size=K.BatchSize
-                                **K.ModelSettings)
-        dummy_input = torch.randn((1, net.input_size[1], net.input_size[2]), dtype=torch.float32)
+        model_init_dict = K.ModelSettings.update({
+                                                'num_input_channels': 1 if K.IOnly else 2,
+                                                'batch_size': K.BatchSize
+                                                })
+        net = InceptionResNet1D(data_manager.classes_num, **K.ModelSettings)
+        # dummy_input = torch.randn((1, net.input_size[1], net.input_size[2]), dtype=torch.float32)
         optimizer = torch.optim.Adam(net.parameters(), lr=K.LearningRate)
-        saver.save(epochID=-1, model=net, optimizer=optimizer)
-
-        start_epoch = 0
+        # saver.save(model_num=-1, model=net, optimizer=optimizer)
+        iteration = 0
+        net.cuda()
     else:
-        net, optimizer = saver.restore(K.RecoverEpochID,
-                                       model_cls=InceptionResNet1D, optimizer_cls=torch.optim.Adam)
-        # ** If recover training, start at recover epoch
-        start_epoch = K.RecoverEpochID + 1
-    net.cuda()
+        net, optimizer = saver.restore(K.LoadModelNum,model_cls=InceptionResNet1D,
+                                       optimizer_cls=torch.optim.Adam, device='cuda')
+        # ** If recover training, start at recover iteration
+        iteration = K.LoadModelNum
     # * Start training
+    # ** Restore epochID
+    batches_num = int(np.ceil(data_manager.train_samples_num / K.BatchSize))
+    start_epoch = int(np.round(iteration / batches_num))
     for epochID in range(start_epoch, K.NumEpochs):
         logger.info('****** Epoch: {}/{} ******'.format(epochID, K.NumEpochs - 1))
 
-        batches_num = int(np.ceil(data_manager.train_samples_num / K.BatchSize))
         # * Init data_manager & Get batches generator
         data_manager.init_epoch()
         train_batches = data_manager.get_train_batches(K.BatchSize)
         # * Init iteration, sum_loss and tester
-        iteration = 0
         sum_loss = 0
         tester.restart()
         # * Process bar
         process_bar = ProcessBar(batches_num)
         for batch_ID, train_batch in enumerate(train_batches):
-            # * Test every log_interval iteration
-            if iteration % K.TrainLogInterval == 0 and iteration != 0:
+            # ! Test every log_interval iteration
+            if iteration % K.TrainLogInterval == 0 and iteration != 0 and \
+                    (not (K.IsRecover and iteration == K.LoadModelNum)):
                 train_loss = sum_loss / K.TrainLogInterval
                 tester.measure()
                 train_accuracy = tester.micro_avg_precision
                 writer.add_scalar('train/loss', train_loss, global_step=iteration)
                 writer.add_scalar('train/accuracy', train_accuracy, global_step=iteration)
+                writer.add_scalars('loss', {'train': train_loss}, global_step=iteration)
+                writer.add_scalars('accuracy', {'train': train_accuracy}, global_step=iteration)
                 # * Reset static loss and tester
                 sum_loss = 0
                 tester.restart()
-                # * If comes to test iteration, test part of test set samples
-                if (iteration / K.TrainLogInterval) % K.TestLogMultiplier == 0:
-                    # * Get test data
-                    test_X, test_Y = data_manager.get_random_test_samples(K.TestSamplesNum)
-                    # Test in eval+no_grad mode
-                    with torch.no_grad:
-                        net.eval()
-                        test_loss, test_accuracy = TestSamples(test_X, test_Y, net, tester)
-                    net.train()
-
-                    writer.add_scalar('test/loss', test_loss, global_step=iteration)
-                    writer.add_scalar('test/accuracy', test_accuracy, global_step=iteration)
-
-                    process_bar.SkipMsg(
-                        '({}/{}) train_loss: {}, train_accuracy: {}, test_loss: {}, test_accuracy: {}'.format(
-                            batch_ID, batches_num - 1, train_loss, train_accuracy, test_loss, test_accuracy)
-                        , logger)
-                    # * Add compare scalar
-                    writer.add_scalars('loss', {'train': train_loss, 'test': test_loss}, global_step=iteration)
-                    writer.add_scalars('accuracy', {'train': train_accuracy, 'test': test_accuracy},
-                                       global_step=iteration)
-                    # * Restart tester for next interval
-                    tester.restart()
-                else:
-                    process_bar.SkipMsg(
-                        'Train: ({}/{}) loss: {}, accuracy: {}'.format(batch_ID, batches_num - 1, train_loss, train_accuracy),
-                        logger)
+                # * Output
+                process_bar.SkipMsg(
+                    'Train: ({}/{}) loss: {}, accuracy: {}'.format(batch_ID, batches_num - 1, train_loss,
+                                                                   train_accuracy), logger)
+            # ! If comes to test iteration, test part of test set samples
+            if iteration % (K.TrainLogInterval * K.TestLogMultiplier) == 0:
+                process_bar.SkipMsg('/*******Now Test the Model*******/')
+                # * Get test data
+                test_X, test_Y = data_manager.get_random_test_samples(K.TestSamplesNum)
+                # Test in eval+no_grad mode
+                with torch.no_grad():
+                    net.eval()
+                    test_loss, test_accuracy = TestSamples(test_X, test_Y, net, tester)
+                net.train()
+                writer.add_scalar('test/loss', test_loss, global_step=iteration)
+                writer.add_scalar('test/accuracy', test_accuracy, global_step=iteration)
+                process_bar.SkipMsg(
+                    'Test: test_loss: {}, test_accuracy: {}'.format(test_loss, test_accuracy), logger)
+                # * Add compare scalar
+                writer.add_scalars('loss', {'test': test_loss}, global_step=iteration)
+                writer.add_scalars('accuracy', {'test': test_accuracy},global_step=iteration)
+                # * Restart tester for next interval
+                tester.restart()
+            # ! If time for take snapshot
+            if iteration % (K.TrainLogInterval * K.TestLogMultiplier * K.SnapshotMultiplier) == 0 and \
+                    not (K.IsRecover and (iteration == K.LoadModelNum)) :
+                process_bar.SkipMsg(
+                    f"Taking Snapshot of current model as {os.path.split(K.SnapshotFileStr.format(iteration))[1]}",
+                                    logger)
+                saver.save(iteration, net, optimizer, model_init_dict=net.model_init_dict)
             # * Process Batch Data
             batch_X, cpu_batch_Y = train_batch
             batch_X = batch_X.reshape(batch_X.shape[0], 1 if K.IOnly else 2, -1)
@@ -143,8 +156,9 @@ if __name__ == '__main__':
             sum_loss += float(loss)
             # * Update bar and iteration
             process_bar.UpdateBar(batch_ID + 1)
+            iteration += 1
         process_bar.Close()
         # ! Save model for this epoch
-        saver.save(epochID=epochID, model=net, optimizer=optimizer,
-                   model_init_dict=net.model_init_dict)
+        # saver.save(model_num=epochID, model=net, optimizer=optimizer,
+        #            model_init_dict=net.model_init_dict)
     writer.close()
